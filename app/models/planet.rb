65637801c4771cc8a1c5090fb418285cfb5e8ae7
auranbuckles <auran59@gmail.com>
2016-10-27 00:35:08 -0400

    added validations no moons and population

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -2,4 +2,8 @@ class Planet < ActiveRecord::Base
 	belongs_to :user
 	has_many :orders
 	has_many :features, through: :orders
+
+	validates :name, presence: true
+	validates :moons, length: { in: 0..8 }
+	validates :population, length: { in: 0..8000000 }
 end
