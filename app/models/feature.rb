1e23ffd0a5c4fe495238249f7a1c639b7d90855f
auranbuckles <auran59@gmail.com>
2016-10-27 01:27:27 -0400

    added validations

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,7 @@
 class Feature < ActiveRecord::Base
 	has_many :orders
 	has_many :planets, through: :orders
+
+	validates :name, presence: true
+	validates :description, length: { maximum: 250 }
 end
