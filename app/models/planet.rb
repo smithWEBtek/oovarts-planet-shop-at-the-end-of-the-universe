d275fffe068d4a0ccaab49c680c126e7d01a9133
auranbuckles <auran59@gmail.com>
2016-10-27 22:34:42 -0400

    created class methods and used them to display info on planet index page

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -6,4 +6,16 @@ class Planet < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true, length: { in: 3..20 }
 	validates :moons, length: { maximum: 8 }
 	validates :population, length: { maximum: 8000000 }
+
+	def self.most_populated
+		order('population desc').limit(1).pluck(:name).join
+	end
+
+	def self.most_moons
+		order('moons desc').limit(1).pluck(:name).join
+	end
+
+	def self.most_expensive
+		order('price desc').limit(1).pluck(:name).join
+	end
 end
