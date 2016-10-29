96255714df0a2bb4b295854fa35ec065b2e5994a
auranbuckles <auran59@gmail.com>
2016-10-29 15:20:28 -0400

    added user authentication methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,27 @@ class User < ActiveRecord::Base
 
   has_many :planets
 
-  validates :name, :email, presence: true
-  validates :name, uniqueness: true, length: { maximum: 20 }
+  def self.current
+    Thread.current[:user]
+  end
+
+  def self.current=(user)
+    Thread.current[:user] = user
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
