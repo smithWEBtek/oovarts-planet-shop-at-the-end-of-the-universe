7557082f5190929c53841bcbd8a5fd88a16d5fb4
auranbuckles <auran59@gmail.com>
2016-10-30 14:13:17 -0400

    changed omniauth to get facebook user name from the 'from_omniauth' method

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.first_name
     end      
   end
 
