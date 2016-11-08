e19ef5389b477781a8143b168892aeda48e348f6
auranbuckles <auran59@gmail.com>
2016-11-08 17:04:26 -0500

    added ajax next button on feature show page

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -22,7 +22,13 @@ $(function() {
     })
 	});
 
-	$("#next-feature").on("click", function(e) {
-
+	$(".js-next").on("click", function() {
+		var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.get("/features/" + nextId + ".json", function(feature) {
+      $(".featureName").text(feature["name"]);
+      $(".featureDescription").text(feature["description"]);
+      // re-set the id to current on the link
+      $(".js-next").attr("data-id", feature["id"]);
+    });
 	});
 });
