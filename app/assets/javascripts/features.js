3c3cd7fe68ebadf24feb297dbaefe9eeb383fb4d
auranbuckles <auran59@gmail.com>
2016-11-08 15:20:29 -0500

    appended order attributes in json format

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -16,10 +16,21 @@ $(function() {
     e.preventDefault();
 
     $.get(this.href).success(function(json) {
-    	var list = $("div.orders-list")
+    	var list = $("div.orders-list table")
     	list.html("")
 
+    	list.append("<tr><th>Order ID</th><th>Feature</th><th>Planet</th><th>Size (fy sq.)</th><th>Price (Pu)</th><th>Customer</th><th>Customer Email</th></tr>");
+
     	json.forEach(function(order) {
+    		list.append("<tr><td>"
+    		 + order.id + "</td>"
+    		 + "<td>" + order.feature.name + "</td>"
+    		 + "<td>" + order.planet.name + "</td>"
+    		 + "<td>" + order.size + "</td>"
+    		 + "<td>" + order.price * 1000 + "</td>"
+    		 + "<td>" + order.planet.user.name + "</td>"
+    		 + "<td>" + order.planet.user.email
+    		 + "</td></tr>")
     		debugger
     	})
     })
