efc5b07883b182a4d99825de78ed68132f39f83b
auranbuckles <auran59@gmail.com>
2016-11-08 15:43:18 -0500

    cleaned up unused code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,15 +7,10 @@ class OrdersController < ApplicationController
     @orders = @feature.orders
     # @orders = Order.all
 
-    # render layout: false
     render json: @orders
   end
 
   def show
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @order }
-    end
   end
 
   def new
