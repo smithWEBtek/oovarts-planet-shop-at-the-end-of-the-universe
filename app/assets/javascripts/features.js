823471c86c25917dc0dc98b43f3597b746a270b1
auranbuckles <auran59@gmail.com>
2016-11-09 13:20:10 -0500

    removed turbolinks to make js work on loading pages

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -21,7 +21,7 @@ $(function() {
 
 	$(".show-orders").on("click", function(e) {
     // prevent response from loading a new page
-    // e.preventDefault();
+    e.preventDefault();
 
     $(this).data('clicked', true);
 
